8c9bc094e9aef6e673711e0600b52ab08ec572c0
Jeswin <jeswinpk@agilehead.com>
2014-07-18 17:47:53 +0530

    First post

---
+++ index.js
@@ -0,0 +1,35 @@
+window.exports = function thunkify(fn){
+  var result = function(){
+    var args = new Array(arguments.length);
+    var ctx = this;
+
+    for(var i = 0; i < args.length; ++i) {
+      args[i] = arguments[i];
+    }
+
+    return function(done){
+      var called;
+
+      args.push(function(){
+        if (called) return;
+        called = true;
+        done.apply(null, arguments);
+      });
+
+      try {
+        fn.apply(ctx, args);
+      } catch (err) {
+        done(err);
+      }
+    }
+  }
+
+  return function*() {
+    try {
+        yield result(arguments);
+    } catch(e) {
+        e._inner = new Error();
+        throw e;
+    }
+  }
+};
