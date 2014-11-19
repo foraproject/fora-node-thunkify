c503b628639c6dbc96a72e777da46e99a0fbbc4b
Jeswin Kumar <jeswinpk@agilehead.com>
2014-11-20 03:49:17 +0530

    conventions cleanup

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 (function() {
-    "use strict"
+
+    "use strict";
 
     module.exports = function thunkify(fn){
-      var thunkFn = function(){
-        var args = Array.prototype.slice.call(arguments);
-        var ctx = this;
-
-        return function(done){
-          var called;
-
-          args.push(function(){
-            if (called) return;
-            called = true;
-            done.apply(null, arguments);
-          });
-
-          try {
-            fn.apply(ctx, args);
-          } catch (err) {
-            done(err);
-          }
-        }
-      }
-
-      return function*() {
-        try {
+        var thunkFn = function(){
             var args = Array.prototype.slice.call(arguments);
-            return yield thunkFn.apply(this, args);
-        } catch(e) {
-            e._inner = new Error();
-            throw e;
-        }
-      }
+            var ctx = this;
+
+            return function(done){
+                var called;
+
+                args.push(function(){
+                    if (called) return;
+                    called = true;
+                    done.apply(null, arguments);
+                });
+
+                try {
+                    fn.apply(ctx, args);
+                } catch (err) {
+                    done(err);
+                }
+            };
+        };
+
+        return function*() {
+            try {
+                var args = Array.prototype.slice.call(arguments);
+                return yield thunkFn.apply(this, args);
+            } catch(e) {
+                e._inner = new Error();
+                throw e;
+            }
+        };
     };
 
 })();
